96581b64c648d8894a0dd707a9c79156e4680d9a
shashwatrajhack <er.shashwatraj@gmail.com>
2022-08-16 22:42:25 +0530

    arithmetic in c++

---
+++ arithmetic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"Enter the value of num1";
+    cin>>num1;
+
+    cout<<"enter the value of num2\n";
+    cin>>num2;
+
+    cout<<"the multiplication is "<<num1*num2;
+    return 0;
+}
