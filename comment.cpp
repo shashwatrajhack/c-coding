d2b9504bb4a81e498d68bfcb96a3d9a9ecca0286
shashwatrajhack <er.shashwatraj@gmail.com>
2022-08-16 21:51:08 +0530

    c++ course

---
+++ comment.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+    cout<<"I am shashwat raj";
+    return 0;
+    //this is the second program in c++
+
+}
---
+++ third.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=4;
+    int b=8;
+    float c=3.14;
+    cout<<"these are the data type. Here the value of a is "<<a<<". the value of b is "<<b;
+    cout<<" .Here the value of c is "<<c;
+    return 0;
+}
