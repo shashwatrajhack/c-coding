dd0dfd755941a78b43adeb87a7804a59be902ae7
shashwatrajhack <er.shashwatraj@gmail.com>
2022-08-16 22:48:19 +0530

    arithmetic operation

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -2,12 +2,22 @@
 using namespace std;
 int main(){
     int num1,num2;
-    cout<<"Enter the value of num1";
+    float num3,num4;
+
+    cout<<"Enter the value of num1\n";
     cin>>num1;
 
     cout<<"enter the value of num2\n";
     cin>>num2;
 
-    cout<<"the multiplication is "<<num1*num2;
+    cout<<"the multiplication is\n "<<num1*num2;
+
+    cout<<"Enter the value of num3\n";
+    cin>>num3;
+
+    cout<<"enter the value iof num4\n";
+    cin>>num4;
+
+    cout<<"the division is "<<num3/num4;
     return 0;
 }
