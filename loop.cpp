88f074b6df470a21f40134719a45b9d59280d9d7
shashwatrajhack <er.shashwatraj@gmail.com>
2023-04-05 23:33:19 +0530

    concepts of c++

---
+++ loop.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    //cout<<"hello worrld"<<endl;
+
+  /* if(n>0){
+        cout<<"N is positive"<<endl;
+
+    }
+    else{
+        cout<<"N is negative"<<endl;
+    }
+    
+
+   a=cin.get();
+
+   cout<<"The value of a is : "<< a  <<endl;
+*/
+int a;
+    cin>>a;
+
+    if(a>0){
+        cout<<"A is positive"<<endl;
+    }
+    else if(a<0){
+        cout<<"A is negative"<<endl;
+     }
+
+    else{
+        cout<<"A is 0"<<endl;
+    }    
+    
+
+
+
+}
